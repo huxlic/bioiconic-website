978ae64cf2f82f3209a0ed9d0865bac66835a63f
huxlic <oladimejihassan03@gmail.com>
2026-08-29 23:58:02 +0100

    refactor Hero and Navbar components for improved layout and styling

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,8 +2,9 @@ import background_image from "../../assets/images/delivery-man-with-packages.jpg
 
 export const Hero = () => {
 	return (
-		<>
-			<section className={"relative w-full rounded-t-2xl bg-fixed h-screen bg-cover bg-center grayscale overflow-hidden"} style={{backgroundImage: `url(${background_image})`}}>
+		<div className={"p-2"}>
+			<section className={"relative w-full rounded-2xl bg-fixed h-screen bg-cover bg-center overflow-hidden"}>
+				<img src={background_image} alt="Bio-Iconic printed products" className=" w-full h-full object-cover" />
 				<div className="absolute inset-0 bg-linear-to-r from-black/85 via-black/20 to-transparent" />
 				
 				<div className="absolute inset-0 pl-10 py-10">
@@ -12,6 +13,6 @@ export const Hero = () => {
 					</p>
 				</div>
 			</section>
-		</>
+		</div>
 	);
 };
--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -4,24 +4,24 @@ import {ArrowUpRight} from "lucide-react";
 export const Navbar = () => {
 	return (
 		<>
-			<header className={"w-full py-4 px-6"}>
+			<header className={"fixed text-white top-0 w-full py-4 px-6 z-50 bg-linear-to-t from-transparent to-black"}>
 				<nav className={"flex items-center justify-between"}>
 					<a className={"text-[20px] font-bold"}  href="/">bio-iconic</a>
 					
-					<div className={"flex text-ink-black items-center gap-2"}>
+					<div className={"flex items-center gap-2"}>
 						<div className={"flex items-center"}>
 							{navLinks.map(({label, ref}) => (
-								<a className={"text-[14px] font-medium flex items-center gap-1 group px-2 "} href={ref} key={label}>
-									<span className={"p-0.75 rounded-full bg-ink-black group-hover:p-2 group-hover:bg-reddish-browm transition-all duration-500"}></span>
+								<a className={"uppercase text-[12px] font-medium flex items-center gap-1 group px-2 "} href={ref} key={label}>
+									<span className={"rounded-full p-1 bg-reddish-browm transition-all duration-500"}></span>
 									{label}
 								</a>
 							))}
 						</div>
 						
-						<button className={"flex items-center gap-2 px-4 py-1 pr-1 bg-reddish-browm hover:bg-dark-walnut transition-colors text-white text-[14px] rounded-full font-medium"}>
+						<button className={"flex items-center gap-2 px-4 py-1 pr-1 bg-reddish-browm hover:bg-dark-walnut transition-colors text-white text-[12px] rounded-full font-medium"}>
 							<span>Contact me</span>
 							<div className={"p-1 rounded-full flex bg-white text-black"}>
-								<ArrowUpRight size={17} />
+								<ArrowUpRight size={16} />
 							</div>
 						</button>
 					</div>
