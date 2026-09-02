4ed0b9753aa05e71a296fe375a4a3bab77a145ff
huxlic <oladimejihassan03@gmail.com>
2026-09-02 11:59:20 +0100

    add KeyStats component with GSAP animations and stats data

--- src/components/sections/KeyStats.tsx
+++ src/components/sections/KeyStats.tsx
@@ -1,6 +1,11 @@
 import {useGSAP} from "@gsap/react";
-import {SplitText} from "gsap/SplitText";
 import gsap from "gsap";
+import {SplitText} from "gsap/SplitText";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import stats from "../../data/stats.ts";
+import achievements from "../../assets/images/achievements.jpg"
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const KeyStats = () => {
 	
@@ -9,21 +14,41 @@ const KeyStats = () => {
 			type: "chars, words"
 		})
 		
+		const stats = gsap.utils.toArray(".stat");
+		
 		gsap.to(keystatDesc.chars, {
 			color: "#0A0A0A",
 			scrollTrigger: {
 				trigger: "#key-stat",
-				start: "top center",
-				end: "bottom 70%",
+				start: "top bottom",
+				end: "top top",
 				scrub: 0.5
 			},
 			stagger: 0.5,
 			ease: "power2.inOut",
 		})
+		
+		gsap.from(stats, {
+			opacity: 0,
+			yPercent: 50,
+			stagger: 0.2,
+			scrollTrigger: {
+				trigger: "#stats-holder",
+				start: "top bottom",
+			}
+		})
+		gsap.from("#achievement-image", {
+			opacity: 0,
+			scrollTrigger: {
+				trigger: "#achievement-image",
+				start: "top bottom",
+			}
+		})
+		
 	}, [])
 	
 	return (
-		<section id={"key-stat"} className={"px-4 md:px-12 py-20"}>
+		<section id={"key-stat"} className={"px-4 md:px-12 py-20 flex flex-col gap-10"}>
 			<div className={"flex flex-col lg:flex-row gap-6"}>
 				<h2 className={"lg:flex-1 font-jetbrains-mono uppercase font-extrabold"}>key stats</h2>
 				<p id="keystat-desc"
@@ -32,8 +57,18 @@ const KeyStats = () => {
 					remembered, and trusted." </p>
 			</div>
 			
-			<div className="">
-			
+			<div className="flex flex-col-reverse lg:flex-row gap-10 lg:items-center">
+				<div id="stats-holder" className="lg:flex-1 grid grid-cols-2 gap-10">
+					{stats.map(({id, number, description}) => (
+						<div key={id} className={"stat"}>
+							<p className={"text-[60px] leading-none font-bold font-jetbrains-mono"}>{number}</p>
+							<span className={"text-faded-black"}>{description}</span>
+						</div>
+					))}
+				</div>
+				
+				<div id="achievement-image" className="lg:flex-1 h-80 bg-cover rounded-2xl grayscale"
+				     style={{backgroundImage: `url(${achievements})`}}></div>
 			</div>
 		</section>
 	)
---
+++ src/data/stats.ts
@@ -0,0 +1,30 @@
+interface Stat {
+	id: number;
+	number: string;
+	description: string;
+}
+
+const stats: Stat[] = [
+	{
+		id: 1,
+		number: "0+",
+		description: "Brands Elevated Over the Years With Purpose",
+	},
+	{
+		id: 2,
+		number: "0+",
+		description: "Projects Delivered Across Print and Apparel",
+	},
+	{
+		id: 3,
+		number: "0+",
+		description: "Businesses Trusted Us With Their Branding",
+	},
+	{
+		id: 4,
+		number: "98%",
+		description: "Client Satisfaction",
+	},
+];
+
+export default stats;
